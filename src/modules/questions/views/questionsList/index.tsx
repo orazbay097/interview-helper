bcf00f7b640a474f6cafc7e39e3c7f39d1a27ab0
Orazbay Ualesh <oualesh@MacBook-Pro-Orazbay.local>
2022-08-04 22:39:25 +0600

    adapitve questions list

--- src/modules/questions/views/questionsList/index.tsx
+++ src/modules/questions/views/questionsList/index.tsx
@@ -1,18 +1,18 @@
+import { Container } from '@mui/system';
 import { Grid, Typography } from '@mui/material';
 import './style.css';
 import { QUESTIONS } from '../../data';
 import QuestionCard from '../../components/questionCard';
-import { Container } from '@mui/system';
 
 
 function QuestionsList() {
   return (
-    <Container className="questions-list" sx={{mt: 1}}>
+    <Container className="questions-list" sx={{ mt: 1 }}>
       <Typography variant='h4'>Questions List</Typography>
 
       <Grid container spacing={2} sx={{ p: 5 }}>
         {QUESTIONS.map((question, index) =>
-          <Grid item key={index} xs={4}>
+          <Grid item key={index} xs={12} sm={6} lg={4} xl={3}>
             <QuestionCard question={question} />
           </Grid>
         )}
